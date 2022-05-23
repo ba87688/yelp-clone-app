cf92b78f1d73f90b1143e8b058cec0bc4977c5db
Jagi <ba87688@gmail.com>
2022-05-23 11:33:57 -0500

    failed Retrofit request. set up Retrofit.

--- app/src/main/java/com/example/yelpcloneapp/MainActivity.kt
+++ app/src/main/java/com/example/yelpcloneapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.yelpcloneapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import com.example.yelpcloneapp.api.YelpService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+private const val TAG = "MainActivity"
+private const val BASE_URL = "https://api.yelp.com/v3/"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val yelpService = retrofit.create(YelpService::class.java)
+        yelpService.searchRestaurants("Avocado Toast", "New York").enqueue(object :Callback<Any>{
+            override fun onResponse(call: Call<Any>, response: Response<Any>) {
+                Log.i(TAG, "onResponse: successe ${response}")
+            }
+
+            override fun onFailure(call: Call<Any>, t: Throwable) {
+                Log.i(TAG, "onFailure: failed with something ${t}")
+            }
+
+        }
+        )
     }
 }
---
+++ app/src/main/java/com/example/yelpcloneapp/api/YelpService.kt
@@ -0,0 +1,14 @@
+package com.example.yelpcloneapp.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface YelpService {
+
+    @GET("businesses/search")
+    fun searchRestaurants(
+        @Query("term") searchTerm:String,
+        @Query("location") location: String
+    ): Call<Any>
+}
